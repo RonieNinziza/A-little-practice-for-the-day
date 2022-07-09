f2b6b3b65cbbe4b9a0e63d7a60a3878dabe0b308
RonieNinziza <96843362+RonieNinziza@users.noreply.github.com>
2022-07-09 23:21:06 +0300

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,7 @@
+# an easy program that calculates my Body Mass Index of a person given their weight and height.
+weight = 65
+height = 159.9 
+
+BMI = (weight / (height**2)) *100
+print(BMI)
+type(BMI)
---
+++ calculator.py
@@ -0,0 +1,41 @@
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years? 10% return each year
+#after one year
+print(100 * 1.1)
+#after two years
+print(100 *1.1*1.1)
+#after seven years
+print(100*1.1*1.1*1.1*1.1*1.1*1.1*1.1) 
+#after seven years(exponentiation method)
+print(100*1.1**7)
+
+# How much is your $100 worth after 7 years? 10% return each year
+#after one year ()
+#Instead of calculating with the actual values, you can use variables instead.
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier**7
+
+# Print out result
+print(result)
+
+
+
+
+
+
